4b21f13b1b5d696e680e10de3a2ca74a787b48ec
James Drabinsky <james.drabinsky@gmail.com>
2022-07-08 16:47:37 -0700

    Added methods for creating a client and sharing a google sheet.

--- daily.py
+++ daily.py
@@ -3,10 +3,32 @@ import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+
+def google_client():
+    scope = ['https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        'daily-accomplishments.json', scope
+    )
+    return gspread.authorize(creds)
+
+
+def create_sheet():
+    sheet = client.create("accomplishments")
+    sheet.share('james.drabinsky@gmail.com', perm_type='user', role='writer')
+
+
+def update_sheet():
+    pass
+
+
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('daily-accomplishments.json', scope)
-# with open('daily-accomplishments.json') as f:
-#     creds = json.load(f)
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    'daily-accomplishments.json', scope
+)
 client = gspread.authorize(creds)
-print(client)
+
+
+sheet = client.open("Daily Accomplishments").sheet1
+sheet.get_all_records()
