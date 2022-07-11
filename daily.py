9efaba5a880c37ccf9c4e0789d753bb27295132c
James Drabinsky <james.drabinsky@gmail.com>
2022-07-11 13:31:49 -0700

    Finished first version of daily.py script.

--- daily.py
+++ daily.py
@@ -1,34 +1,46 @@
-import json
+from datetime import date
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-def google_client():
-    scope = ['https://www.googleapis.com/auth/spreadsheets',
-             "https://www.googleapis.com/auth/drive"]
+def google_client() -> gspread.client.Client:
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
     creds = ServiceAccountCredentials.from_json_keyfile_name(
-        'daily-accomplishments.json', scope
+        "daily-accomplishments.json", scope
     )
     return gspread.authorize(creds)
 
 
-def create_sheet():
-    sheet = client.create("accomplishments")
-    sheet.share('james.drabinsky@gmail.com', perm_type='user', role='writer')
+def create_sheet(client: gspread.client.Client) -> None:
+    sheet = client.create("Daily Accomplishments")
+    sheet.share("james.drabinsky@gmail.com", perm_type="user", role="writer")
+
+
+def list_accomplishments():
+    items = []
+    while True:
+        item = input()
+        if str(item).lower() == "stop":
+            break
+        else:
+            items.append(str(item))
+    return "\n".join(items)
 
 
 def update_sheet():
-    pass
+    client = google_client()
+    sheet = client.open("Daily Accomplishments").get_worksheet(0)
+    max_row = sheet.row_count
+    n = next(num for num in range(1, max_row) if not sheet.row_values(num))
+    sheet.update_acell(f"A{n}", f"{date.today():%Y-%m-%d}")
+    sheet.update_acell(f"B{n}", list_accomplishments())
 
 
-scope = ['https://www.googleapis.com/auth/spreadsheets',
-         "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    'daily-accomplishments.json', scope
-)
-client = gspread.authorize(creds)
-
-
-sheet = client.open("Daily Accomplishments").sheet1
-sheet.get_all_records()
+if __name__ == "__main__":
+    print("\nList your daily accomplishments below:\n")
+    update_sheet()
+    print("\nAll done!  Thanks!\n")
